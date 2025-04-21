29249b19d77b70dad0752710457839c243b8e60c
BLSSMA <162845981+BLSSMA@users.noreply.github.com>
2025-04-21 17:52:01 -0400

    added titles to links

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -11,13 +11,13 @@ const WelcomePage = () => {
                 <h1 className="typed-out">✨WELCOME✨</h1>
             </div>
             <div className="StoreButton">
-                <a href="https://www.little-haunted-designs.com/" className="StoreLink" target="_blank" rel="noreferrer">STORE</a>
+                <a href="https://www.little-haunted-designs.com/" className="StoreLink" title="https://www.little-haunted-designs.com/"target="_blank" rel="noreferrer">STORE</a>
             </div>
             <div className="EtsyButton">
-                <a href="https://littlehaunteddesigns.etsy.com" className="EtsyLink" target="_blank" rel="noreferrer">ETSY</a>
+                <a href="https://littlehaunteddesigns.etsy.com" className="EtsyLink" title="https://littlehaunteddesigns.etsy.com" target="_blank" rel="noreferrer">ETSY</a>
             </div>
             <div className="InstagramButton">
-                <a href="https://www.instagram.com/haunted_design/" className="PortfolioLink" target="_blank" rel="noreferrer">INSTAGRAM</a>
+                <a href="https://www.instagram.com/haunted_design/" className="PortfolioLink" title="https://www.instagram.com/haunted_design/" target="_blank" rel="noreferrer">INSTAGRAM</a>
             </div>
             <div className="ghostImage">
                 <img src={ghost} alt="ghost" className="d-flex"/>
